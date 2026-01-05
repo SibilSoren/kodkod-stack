c3e6786828b59db04cbac22da938936dfc1befad
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-01-05 20:02:15 +0530

    feat: setup nextra documentation structure

---
+++ docs/theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>antstack-js</span>,
+  project: {
+    link: 'https://github.com/SibilSoren/antstack-js'
+  },
+  docsRepositoryBase: 'https://github.com/SibilSoren/antstack-js/tree/main/docs',
+  footer: {
+    text: 'antstack-js Documentation'
+  }
+}
