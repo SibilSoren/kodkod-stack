06c25c7ab7acc7058cb213b61c8979cfc0bddf35
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-01-06 11:39:54 +0530

    fix(templates): fix invalid export syntax in express user.routes.ts
    The export statement used object property syntax instead of ES6 export renaming.
    Changed from 'export { userRoutes: router }' to 'export { router as userRoutes }'

--- templates/frameworks/express/src/api/routes/user.routes.ts
+++ templates/frameworks/express/src/api/routes/user.routes.ts
@@ -15,4 +15,4 @@ router.get('/', (req, res, next) => userController.getAll(req, res, next));
 router.get('/:id', (req, res, next) => userController.getById(req, res, next));
 router.post('/', (req, res, next) => userController.create(req, res, next));
 
-export { userRoutes: router };
+export { router as userRoutes };
